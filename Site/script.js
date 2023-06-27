6af24c24146524f136d834ca21a3a60316880ad1
AkaBerry <122053813+AkaBerry@users.noreply.github.com>
2023-06-27 11:10:57 -0300

    Create script.js

---
+++ Site/script.js
@@ -0,0 +1,19 @@
+// Array para armazenar os itens do carrinho
+let cartItems = [];
+
+// Função para adicionar um item ao carrinho
+function addToCart(productId) {
+    // Lógica para adicionar o item ao carrinho
+    // ...
+}
+
+// Função para atualizar o carrinho na página
+function updateCart() {
+    // Lógica para atualizar o carrinho na página
+    // ...
+}
+
+// Função de exemplo em JavaScript
+function exibirMensagem() {
+    console.log("Olá! Esta é uma mensagem de exemplo.");
+}
